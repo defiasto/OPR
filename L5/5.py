8f7764636ba70454d697ffddce5cfd44722db629
defiasto <defias.to@yandex.ru>
2013-09-16 09:55:34 +0300

    5 task is corrected

--- L5/5.py
+++ L5/5.py
@@ -1,7 +1,11 @@
 def isLeap(year):
-	if year % 4 == 0:
+	d4 = not(year % 4)
+	d100 = year % 100
+	d400 = not(year % 400)
+	
+	if (d4 and d100) or d400:
 		return True
 	else:
 		return False
-		
-print(isLeap(2002))
+
+print(isLeap(2004))
